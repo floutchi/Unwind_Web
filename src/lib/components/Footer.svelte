00f484117f621623d760c5f094ed4d7eb7fec8f8
Eppe Quentin <q.eppe@student.helmo.be>
2023-10-24 10:25:33 +0200

    Added footer

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,3 @@
+<footer class="py-10 mt-10 border-t-2 border-gray-200 text-center">
+  <p>&copy; 2023 Quentin Eppe & Florian Detiffe. Tous droits réservés.</p>
+</footer>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,7 @@
   import Navbar from "$lib/components/Navbar.svelte";
   import { onMount } from "svelte";
   import "../app.css";
+  import Footer from "$lib/components/Footer.svelte";
 
   onMount(async () => await loadAuth());
 </script>
@@ -10,4 +11,5 @@
 <div class="px-10 md:px-20 lg:px-40 min-h-screen bg-white font-lexend">
   <Navbar />
   <slot />
+  <Footer />
 </div>
