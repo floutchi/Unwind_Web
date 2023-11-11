8309de852839a29dc5906928391261d7d29a7900
Eppe Quentin <q.eppe@student.helmo.be>
2023-11-11 17:09:12 +0100

    Added configuration entry to support dynamic routes with prerendering

--- svelte.config.js
+++ svelte.config.js
@@ -13,6 +13,10 @@ const config = {
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter(),
 
+    prerender: {
+      entries: ["*", "/periods/1", "/periods/1/invite"],
+    },
+
     paths: {
       base: "/~q210126/unwind",
     },
