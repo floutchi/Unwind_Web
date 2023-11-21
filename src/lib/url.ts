1714e980c43af33e46ccb503ac9af94e524ac2ee
LaBaguetteDev <fdetiffe@gmail.com>
2023-11-21 11:33:46 +0100

    Changement BASE_URL

--- src/lib/url.ts
+++ src/lib/url.ts
@@ -1,3 +1,3 @@
-//export const BASE_URL = "https://studapps.cg.helmo.be:5011/REST_DETI_EPPE";
+export const BASE_URL = "https://studapps.cg.helmo.be:5011/REST_DETI_EPPE";
 export const BASE_URL_WS = "ws://studapps.cg.helmo.be:5011/REST_DETI_EPPE/ws";
-export const BASE_URL = "http://localhost:8080";
+//export const BASE_URL = "http://localhost:8080";
